14538689ec113f35c8e50005f97825d180aa051d
flurin-conradin <f.conradin@gmail.com>
2022-11-09 15:26:03 +0100

    update to juntagrico 1.5.4

--- juntagrico_depot_management/urls.py
+++ juntagrico_depot_management/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+from django.urls import re_path
 from juntagrico_depot_management import views
 
 urlpatterns = [
-    url(r'^dm/depot/change/overview$', views.depot_change_overview),
-    url(r'^dm/depot/change/(?P<subscription_id>.*?)/', views.activate_future_depot),
+    re_path(r'^dm/depot/change/overview$', views.depot_change_overview),
+    re_path(r'^dm/depot/change/(?P<subscription_id>.*?)/', views.activate_future_depot),
 ]
